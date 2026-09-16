b42430d62c702c798b0a9d69461a3b2b670140b3
Virender Singh Rawat <100502321+virenderrawatgit@users.noreply.github.com>
2026-09-16 16:31:16 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,19 +1,19 @@
 variable "project" {
   description = "this is gcp project-id"
   type        = string
-  default     = "rock-loop-449918-b0"
+  default     = "gcp-aug8"
 }
 
 variable "region" {
   description = "this is gcp region"
   type        = string
-  default     = "africa-south1"
+  default     = "us-central1"
 }
 
 variable "zone" {
   description = "this is gcp zone"
   type        = string
-  default     = "africa-south1-a"
+  default     = "us-central1-a"
 }
 
 variable "K8s_version" {
